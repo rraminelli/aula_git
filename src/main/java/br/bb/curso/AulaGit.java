c673cc9452a50785da1e015acff746842dd0b025
rraminelli <rraminelli@gmail.com>
2022-06-06 21:26:08 +0200

    mensagem alterada

--- src/main/java/br/bb/curso/AulaGit.java
+++ src/main/java/br/bb/curso/AulaGit.java
@@ -1,10 +1,12 @@
 package br.bb.curso;
 
+import java.time.LocalDate;
+
 public class AulaGit {
 
     public static void main(String[] args) {
 
-        System.out.println("Bem vindo na Aula sobre Git");
+        System.out.println("Bem vindo na Aula sobre Git " + LocalDate.now());
         
         
     }
